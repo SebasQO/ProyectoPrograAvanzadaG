ed5e8431be228903e81bc6c605cd552619379302
SebasQO <98502805+SebasQO@users.noreply.github.com>
2024-08-20 20:41:11 -0600

    3Commit SebasQO
    Se crea un dashboard donde se va a visualizar informacion general
    
    Pendiente agregarle una tabla

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProyectoPrograAvanzadaG.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,18 @@ namespace ProyectoPrograAvanzadaG.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            var model = new DashboardViewModel
+            {
+                TotalUsuarios = db.Usuarios.Count(),
+                TotalProductos = db.Productos.Count(),
+                TotalPedidosRecientes = db.Carritos.Count(),
+                TotalReseñasPendientes = db.Resenas.Count()
+            };
+            return View(model);
         }
     }
 }
---
+++ Models/DashboardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ProyectoPrograAvanzadaG.Models
+{
+    public class DashboardViewModel
+    {
+        public int TotalUsuarios { get; set; }
+        public int TotalProductos { get; set; }
+        public int TotalPedidosRecientes { get; set; }
+        public int TotalReseñasPendientes { get; set; }
+    }
+}
