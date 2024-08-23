37d44e34f862335d3b7474942c96022a3bfcd564
Esteban Ugalde Rojas <77808945+eugalde60459ufideaccr@users.noreply.github.com>
2024-08-23 12:31:31 -0600

    Create InitialCreateClientesProductos2.cs

---
+++ Migrations/202408220001185_InitialCreateClientesProductos2.cs
@@ -0,0 +1,16 @@
+namespace ProyectoPrograAvanzadaG.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreateClientesProductos2 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
